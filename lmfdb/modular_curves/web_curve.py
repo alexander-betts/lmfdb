f15d1187e42aea8fab113a3cb49660ac7de6e6c3
alexander-betts <l.alexander.betts@gmail.com>
2022-11-05 19:18:50 +0000

    Fixed some errant instances of \textsf Lines starting

--- lmfdb/modular_curves/web_curve.py
+++ lmfdb/modular_curves/web_curve.py
@@ -265,11 +265,11 @@ class WebModCurve(WebObj):
     def qtwist_description(self):
         if self.contains_negative_one:
             if len(self.qtwists) > 1:
-                return r"$\textsf{yes}\quad$ (see %s for level structures without $-I$)"%(', '.join([modcurve_link(label) for label in self.qtwists[1:]]))
+                return r"yes  (see %s for level structures without $-I$)"%(', '.join([modcurve_link(label) for label in self.qtwists[1:]]))
             else:
-                return r"$\textsf{yes}$"
+                return r"yes"
         else:
-            return r"$\textsf{no}\quad$ (see %s for the level structure with $-I$)"%(modcurve_link(self.qtwists[0]))
+            return r"no  (see %s for the level structure with $-I$)"%(modcurve_link(self.qtwists[0]))
 
     @lazy_attribute
     def cusp_display(self):
